9a5f0db1f658b1de2d328aebb549d38811d62921
Jeff Ober <jober@ziprecruiter.com>
2020-06-24 11:09:05 -0400

    Minor fixups for bench.pl

--- bench.pl
+++ bench.pl
@@ -2,24 +2,27 @@ use strict;
 use warnings;
 
 use Benchmark qw(:all);
+use POSIX qw(floor);
 use SkewHeap::PP;
 use SkewHeap;
 
 for my $size (50_000, 100_000, 500_000) {
+  my $count = floor(10_000_000_000 / $size);
+  my $item = floor($size / 2);
+
+  print "\n";
+  print "------------------------------------------------------------------------------\n";
+  print "- put() and take() 1 item with heap containing $size nodes ($count x times)   \n";
+  print "------------------------------------------------------------------------------\n";
+
   my $pp = skew{ $_[0] <=> $_[1] };
   skew_put($pp, 0..$size);
 
   my $xs = skewheap{ $a <=> $b };
   $xs->put(0..$size);
 
-  my $item = $size / 2;
-
-  print "\n";
-  print "------------------------------------------------------------------------------\n";
-  print "- put() and take() 1 item with skew heap containing $size nodes\n";
-  print "------------------------------------------------------------------------------\n";
-  cmpthese 100_000, {
+  cmpthese $count, {
     'pp' => sub{ skew_put $pp, $item; my $t = skew_take $pp; },
-    'xs' => sub{ SkewHeap::put $xs, $item; my $t = SkewHeap::take $xs; },
+    'xs' => sub{ $xs->put($item); my $t = $xs->take; },
   };
 }
